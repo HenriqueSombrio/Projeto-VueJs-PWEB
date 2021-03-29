33d8cd1eaf46b882c2e0eddf8af1aad354c2c6b7
unknown <henriquesombrio3@gmail.com>
2021-03-29 20:16:24 -0300

    Primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <p>Usando vuejs - Seu nome {{ name }}</p>
+          <p>Idade multuplicada por 3 = {{ minhaIdade(24) }}</p>
+          <p>Teste: {{ numberRandom(2) }}</p>
+          <label for="imgGoogle">Endereço de imagem: </label>
+          <input
+            type="text"
+            placeholder="Informe o endereço da imagem"
+            name="imgGoogle"
+            v-model="linkMsg"
+          />
+          <br /><img v-bind:src="linkMsg" height="400" />
+          <div>
+            <input type="text" v-bind:value="name" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      name: "Henrique Sombrio",
+      message: "hello word",
+      linkMsg: "",
+    };
+  },
+  methods: {
+    minhaIdade: function (idade) {
+      return idade * 3;
+    },
+    numberRandom: function () {
+      return Math.random();
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #fff;
 }
 </style>
