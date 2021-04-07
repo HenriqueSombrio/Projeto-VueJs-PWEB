acc206db93d4ab4e98ac247fcd14078891caeb86
Henrique Sombrio <henriquesombrio3@gmail.com>
2021-04-06 22:25:44 -0300

    Alterado validação do formulario

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <h1>Registrar Reclamação</h1>
-    <div class="conteudo">
-      <form class="painel">
+    <div class="conteudo" v-if="enviou === false">
+      <form class="painel" >
         <div class="cabecalho">Formulário</div>
 
         <rotulo nome="E-mail">
@@ -14,7 +14,7 @@
         </rotulo>
 
         <rotulo nome="Idade">
-          <input type="number" v-model.number="usuario.idade"/>
+          <input type="number" v-model.number="usuario.idade" />
         </rotulo>
 
         <rotulo nome="Mensagem">
@@ -23,86 +23,132 @@
 
         <rotulo nome="Características do Problema">
           <span class="mr-4"
-            ><input type="checkbox" value="reproduzivel" /> Reproduzível</span
+            ><input
+              type="checkbox"
+              v-model="caracteristica"
+              value="reproduzivel"
+            />
+            Reproduzível</span
           >
           <span
-            ><input type="checkbox" value="intermitente" /> Intermitente</span
+            ><input
+              type="checkbox"
+              v-model="caracteristica"
+              value="intermitente"
+            />
+            Intermitente</span
           >
         </rotulo>
 
         <rotulo nome="Qual produto?">
-          <span class="mr-4"><input type="radio" /> Web</span>
-          <span class="mr-4"><input type="radio" /> Mobile</span>
-          <span><input type="radio" /> Outro</span>
+          <span class="mr-4"
+            ><input type="radio" value="Web" v-model="produto" /> Web</span
+          >
+          <span class="mr-4"
+            ><input type="radio" value="Mobile" v-model="produto" />
+            Mobile</span
+          >
+          <span
+            ><input type="radio" v-model="produto" value="Outro" /> Outro</span
+          >
         </rotulo>
 
         <rotulo nome="Prioridade">
-          <select name="" id="">
-            <option></option>
+          <select name="" id="" v-model="prioridade">
+            <option v-for="p in prioridades" :key="p.id" :value="p.id">
+              {{ p.nome }}
+            </option>
           </select>
         </rotulo>
 
         <rotulo nome="Primeira Reclamação?">
-          <escolha />
+          <escolha v-model="primeira"/>
         </rotulo>
         <hr />
-        <button>Enviar</button>
+        <button type="button" @click.prevent="enviar">Enviar</button>
       </form>
+    </div>
+    <div class="painel" v-if="enviou === true">
+      <div class="cabecalho">Resultado</div>
 
-      <div class="painel">
-        <div class="cabecalho">Resultado</div>
+      <rotulo nome="E-mail">
+        <span>{{ usuario.email }}</span>
+      </rotulo>
 
-        <rotulo nome="E-mail">
-          <span>{{usuario.email}}</span>
-        </rotulo>
+      <rotulo nome="Senha">
+        <span>{{ usuario.senha }}</span>
+      </rotulo>
 
-        <rotulo nome="Senha">
-          <span>{{usuario.senha}}</span>
-        </rotulo>
+      <rotulo nome="Idade">
+        <span>{{ usuario.idade }}</span>
+      </rotulo>
 
-        <rotulo nome="Idade">
-          <span>{{usuario.idade}}</span>
-        </rotulo>
+      <rotulo nome="Mensagem">
+        <span style="white-space: pre">{{ mensagem }}</span>
+      </rotulo>
 
-        <rotulo nome="Mensagem">
-          <span style="white-space: pre">{{mensagem}}</span>
-        </rotulo>
+      <rotulo nome="Marque as Opções">
+        <ul>
+          <li v-for="c in caracteristica" :key="c">
+            {{ c }}
+          </li>
+        </ul>
+      </rotulo>
 
-        <rotulo nome="Marque as Opções">
-          <span>???</span>
-        </rotulo>
+      <rotulo nome="Qual produto?">
+        <span>{{ produto }}</span>
+      </rotulo>
 
-        <rotulo nome="Qual produto?">
-          <span>???</span>
-        </rotulo>
+      <rotulo nome="Prioridade">
+        <span>???</span>
+      </rotulo>
 
-        <rotulo nome="Prioridade">
-          <span>???</span>
-        </rotulo>
-
-        <rotulo nome="Primeira Reclamação?">
-          <span>???</span>
-        </rotulo>
-      </div>
+      <rotulo nome="Primeira Reclamação?">
+        <span>???</span>
+      </rotulo>
     </div>
   </div>
 </template>
 
 <script>
-import escolha from "./components/escolha.vue";
-import rotulo from "./components/rotulo.vue";
+import escolha from "./components/escolha";
+import rotulo from "./components/rotulo";
 export default {
   components: { rotulo, escolha },
 
   data() {
     return {
-      mensagem: '',
+      mensagem: "",
+      caracteristica: [],
+      produto: "Web",
       usuario: {
-        email: '',
-        senha: '',
+        email: "",
+        senha: "",
         idade: 25,
       },
+      prioridade: 1,
+      prioridades: [
+        {
+          id: 1,
+          nome: "Alta",
+        },
+        {
+          id: 2,
+          nome: "Média",
+        },
+        {
+          id: 3,
+          nome: "Baixa",
+        },
+      ],
+      primeira: true,
+      enviou: false,
     };
+  },
+  methods: {
+    enviar() {
+      this.enviou = true;
+    },
   },
 };
 </script>
